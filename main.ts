901ff4c171405c14506e5ae88e133bb785c89819
Adri314 <34138145+Adri314@users.noreply.github.com>
2020-04-10 14:55:24 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,410 @@
+namespace SpriteKind {
+    export const Goal = SpriteKind.create()
+}
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+b d d b b b b b b b b b b b b b 
+b d d b b b b b b b b b b b b b 
+b d d b b b b b b b b b b b b d 
+b d d b b b b b b b b b b b b d 
+b d d b b b b b b b b b b b b d 
+b d d b b b b b b b b b b b b d 
+b d d b b b b b b b b b b b b b 
+b d d b b b b b b b b b b b b b 
+b d d b b b b b b b b b b b b b 
+b d d b b b b b b b b b b b b b 
+b d d b b b b b b b b b b b b d 
+b d d b b b b b b b b b b b b d 
+b d d b b b b b b b b b b b b d 
+b d d b b b b b b b b b b b b d 
+b d d b b b b b b b b b b b b b 
+b d d b b b b b b b b b b b b b 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+b b b b b b b b b b b b b d d b 
+b b b b b b b b b b b b b d d b 
+d b b b b b b b b b b b b d d b 
+d b b b b b b b b b b b b d d b 
+d b b b b b b b b b b b b d d b 
+d b b b b b b b b b b b b d d b 
+b b b b b b b b b b b b b d d b 
+b b b b b b b b b b b b b d d b 
+b b b b b b b b b b b b b d d b 
+b b b b b b b b b b b b b d d b 
+d b b b b b b b b b b b b d d b 
+d b b b b b b b b b b b b d d b 
+d b b b b b b b b b b b b d d b 
+d b b b b b b b b b b b b d d b 
+b b b b b b b b b b b b b d d b 
+b b b b b b b b b b b b b d d b 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f 1 f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+f f f f f f f f f f f f f f f f 
+f f 1 f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`
+    //% blockIdentity=images._tile
+    export const tile7 = img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f 1 f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`
+    //% blockIdentity=images._tile
+    export const tile8 = img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f 1 f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    car2.vy += acceleration
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Goal, function (sprite, otherSprite) {
+    if (info.player1.score() > info.player2.score()) {
+        info.setScore(info.player1.score())
+    } else {
+        info.setScore(info.player2.score())
+    }
+    game.over(true)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    car1.vy += acceleration
+})
+let car2: Sprite = null
+let car1: Sprite = null
+let acceleration = 0
+tiles.setTilemap(tiles.createTilemap(
+            hex`0a0050000303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030403030303030303030303`,
+            img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8],
+            TileScale.Sixteen
+        ))
+for (let index = 0; index < 300; index++) {
+    tiles.setTileAt(tiles.getTileLocation(Math.randomRange(0, 10), Math.randomRange(0, 80)), myTiles.tile5)
+    tiles.setTileAt(tiles.getTileLocation(Math.randomRange(0, 10), Math.randomRange(0, 80)), myTiles.tile6)
+    tiles.setTileAt(tiles.getTileLocation(Math.randomRange(0, 10), Math.randomRange(0, 80)), myTiles.tile7)
+    tiles.setTileAt(tiles.getTileLocation(Math.randomRange(0, 10), Math.randomRange(0, 80)), myTiles.tile8)
+}
+acceleration = -5
+let friction = 1
+car1 = sprites.create(img`
+. . . . . . . c 4 . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . e 4 . . . . . . . 
+. . . . . . e e 5 2 . . . . . . 
+. . . . . . e 4 5 2 . . . . . . 
+. . . . . c c c 2 2 2 . . . . . 
+. . . . e e 4 4 4 5 2 2 . . . . 
+. . e f f f c c 2 2 f f 2 2 . . 
+. e e e e 2 2 4 4 4 4 5 4 2 2 . 
+e e e e e e 2 2 4 4 4 5 4 4 2 2 
+e e e e e e 2 2 4 4 4 4 5 4 2 2 
+. . . . . 2 4 5 5 4 2 . . . . . 
+. . . . . . 2 5 4 2 . . . . . . 
+. . . . . . . 2 4 . . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . . . 2 . . . . . . . 
+`, SpriteKind.Player)
+animation.runImageAnimation(
+car1,
+[img`
+. . . . . . . c 4 . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . e 4 . . . . . . . 
+. . . . . . e e 5 2 . . . . . . 
+. . . . . . e 4 5 2 . . . . . . 
+. . . . . c c c 2 2 2 . . . . . 
+. . . . e e 4 4 4 5 2 2 . . . . 
+. . e f f f c c 2 2 f f 2 2 . . 
+. e e e e 2 2 4 4 4 4 5 4 2 2 . 
+e e e e e e 2 2 4 4 4 5 4 4 2 2 
+e e e e e e 2 2 4 4 4 4 5 4 2 2 
+. . . . . 2 4 9 5 4 2 . . . . . 
+. . . . . . 2 5 4 2 . . . . . . 
+. . . . . . . 2 4 . . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . . . 2 . . . . . . . 
+`,img`
+. . . . . . . c 4 . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . e 4 . . . . . . . 
+. . . . . . e e 5 2 . . . . . . 
+. . . . . . e 4 5 2 . . . . . . 
+. . . . . c c c 2 2 2 . . . . . 
+. . . . e e 4 4 4 5 2 2 . . . . 
+. . e f f f c c 2 2 f f 2 2 . . 
+. e e e e 2 2 4 4 4 4 5 4 2 2 . 
+e e e e e e 2 2 4 4 4 5 4 4 2 2 
+e e e e e e 2 2 4 4 4 4 5 4 2 2 
+. . . . . 2 4 5 9 4 2 . . . . . 
+. . . . . . 2 4 5 2 . . . . . . 
+. . . . . . . 4 2 . . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . . 2 . . . . . . . . 
+`],
+200,
+true
+)
+tiles.placeOnTile(car1, tiles.getTileLocation(3, 79))
+car2 = sprites.create(img`
+. . . . . . . b 6 . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . c 6 . . . . . . . 
+. . . . . . c c 9 8 . . . . . . 
+. . . . . . c 6 9 8 . . . . . . 
+. . . . . b b b 8 8 8 . . . . . 
+. . . . c c 6 6 6 9 8 8 . . . . 
+. . c f f f b b 2 2 f f 8 8 . . 
+. c c c c 8 8 6 6 6 6 9 6 8 8 . 
+c c c c c c 8 8 6 6 6 9 6 6 8 8 
+c c c c c c 8 8 6 6 6 6 9 6 8 8 
+. . . . . 2 4 5 5 4 2 . . . . . 
+. . . . . . 2 5 4 2 . . . . . . 
+. . . . . . . 2 4 . . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . . . 2 . . . . . . . 
+`, SpriteKind.Player)
+animation.runImageAnimation(
+car2,
+[img`
+. . . . . . . b 6 . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . c 6 . . . . . . . 
+. . . . . . c c 9 8 . . . . . . 
+. . . . . . c 6 9 8 . . . . . . 
+. . . . . b b b 8 8 8 . . . . . 
+. . . . c c 6 6 6 9 8 8 . . . . 
+. . c f f f b b 2 2 f f 8 8 . . 
+. c c c c 8 8 6 6 6 6 9 6 8 8 . 
+c c c c c c 8 8 6 6 6 9 6 6 8 8 
+c c c c c c 8 8 6 6 6 6 9 6 8 8 
+. . . . . 2 4 9 5 4 2 . . . . . 
+. . . . . . 2 5 4 2 . . . . . . 
+. . . . . . . 2 4 . . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . . . 2 . . . . . . . 
+`,img`
+. . . . . . . b 6 . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . c 6 . . . . . . . 
+. . . . . . c c 9 8 . . . . . . 
+. . . . . . c 6 9 8 . . . . . . 
+. . . . . b b b 8 8 8 . . . . . 
+. . . . c c 6 6 6 9 8 8 . . . . 
+. . c f f f b b 2 2 f f 8 8 . . 
+. c c c c 8 8 6 6 6 6 9 6 8 8 . 
+c c c c c c 8 8 6 6 6 9 6 6 8 8 
+c c c c c c 8 8 6 6 6 6 9 6 8 8 
+. . . . . 2 4 5 9 4 2 . . . . . 
+. . . . . . 2 4 5 2 . . . . . . 
+. . . . . . . 4 2 . . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . . 2 . . . . . . . . 
+`],
+200,
+true
+)
+tiles.placeOnTile(car2, tiles.getTileLocation(6, 79))
+let end = sprites.create(img`
+f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+`, SpriteKind.Goal)
+end.setPosition(80, 1)
+game.splash("Button Masher Race", "in Space!")
+game.splash("P1: Press A", "P2: Press B")
+game.onUpdateInterval(200, function () {
+    if (!(controller.A.isPressed()) && car1.vy < 0) {
+        car1.vy += friction
+    }
+    if (!(controller.B.isPressed()) && car2.vy < 0) {
+        car2.vy += friction
+    }
+})
+game.onUpdate(function () {
+    info.player1.setScore(car2.y - car1.y)
+    info.player2.setScore(car1.y - car2.y)
+    if (info.player1.score() > info.player2.score()) {
+        scene.cameraFollowSprite(car1)
+    } else {
+        scene.cameraFollowSprite(car2)
+    }
+})
